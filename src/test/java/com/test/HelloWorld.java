68c53030bc1b455e4c5558662d20300532250cb4
wangguan5188 <572119023@qq.com>
2016-07-02 15:06:38 +0800

    王冠，我爱你。王冠，我爱你。王冠，我爱你。我是天宝

---
+++ src/test/java/com/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("王冠，我爱你。");
+		System.out.println("王冠，我爱你。");
+		System.out.println("王冠，我爱你。");
+	}
+
+}
